48821cfd33fcdab1c46cff82b61843a0b61ed5ce
nitinthite <nitin.thite@itwox.com>
2021-04-30 20:58:02 +0530

    first push

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Avatar, Button, TextInput } from 'react-native-paper';
+import { red100 } from 'react-native-paper/lib/typescript/styles/colors';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+            {/* <Avatar.Icon style={{ backgroundColor: "#00C33F" }} color={"#FFFFFF"} icon="check" /> */}
+            <View style={{ width: "100%" }}>
+            <TextInput
+                        mode={"flat"}
+                        label="E-mail"
+                        // left={<TextInput.Icon name="email-outline" color="#FF00FF" />}
+                        keyboardType={"email-address"}
+                        returnKeyType={"done"}
+                        blurOnSubmit={true}
+                    />
+            </View>
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
